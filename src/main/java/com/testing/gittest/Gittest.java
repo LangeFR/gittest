9b14381f41338cc205da78c7c64de91f3c5bd727
LangeFR <mdjvanegasc@gmail.com>
2024-11-03 00:41:34 -0500

    First commit test1

---
+++ src/main/java/com/testing/gittest/Gittest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.testing.gittest;
+
+/**
+ *
+ * @author Mateo
+ */
+public class Gittest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
